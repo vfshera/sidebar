aee113fe41338d94b952f8bb6a204a005d2bf2f9
Franklin Shera <fshera96@gmail.com>
2022-11-01 20:54:11 +0300

    left sidebar

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const leftSidebar = document.querySelector("#leftSidebar");
+const headerTitle = document.querySelector("#headerTitle");
+const menuTitles = Array.from(document.querySelectorAll(".menu-title"));
+
+function toggleNav(toggleBtn) {
+  if (leftSidebar?.getAttribute("data-expanded") === "true") {
+    leftSidebar?.classList.remove("w-72");
+    leftSidebar?.classList.add("w-20");
+
+    leftSidebar?.setAttribute("data-expanded", "false");
+  } else {
+    leftSidebar?.classList.add("w-72");
+    leftSidebar?.classList.remove("w-20");
+    leftSidebar?.setAttribute("data-expanded", "true");
+  }
+
+  // rotate toggle
+  toggleBtn?.classList.toggle("rotate-180");
+
+  // hide header title
+  headerTitle?.classList.toggle("scale-0");
+
+  // hide title
+  menuTitles.forEach((title) => title.classList.toggle("hidden"));
+}
